31e29610c430adcb5223571cde046fa7525d862a
Michalina Janowska <janowska.michalina208@gmail.com>
2025-05-29 20:43:44 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer">
+      Michalina Janowska
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className="home">
+      <section className="home__section home__section--intro">
+        <h1 className="home__title">
+          Why is Minecraft worth playing?
+        </h1>
+        <p className="home__text">
+          Minecraft is a game full of endless creativity and adventure. Whether you're building incredible structures, exploring vast worlds, or simply hanging out with friends, Minecraft offers freedom and fun like no other game. It's relaxing, educational, and endlessly imaginative a true sandbox experience.
+        </p>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/1.png" alt="Landscape 1" className="home__image" />
+          <img src="/src/img/2.png" alt="Landscape 2" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              Minecraft is absolutely beautiful! You can build awesome things, enjoy stunning views, and even go on fun adventures with animals. It’s a world full of wonder and creativity!
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/3.png" alt="Funny build 1" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              Some builds are just hilarious! Like the U.S. President's office hidden in our underground base, my pink bedroom, or a weird beach under Oliwier’s house. And yes – it's all thanks to awesome mods. Super fun!
+            </p>
+          </div>
+          <img src="/src/img/4.png" alt="Funny build 2" className="home__image" />
+          <img src="/src/img/5.png" alt="Funny build 3" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/6.jpeg" alt="Adventure fall" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              Going on an adventure and getting pushed into lava by someone like MikoJump... not fun 😩 But yeah, it happens. That pain is real.
+            </p>
+          </div>
+          <img src="/src/img/7.jpeg" alt="Text strip" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <div className="home__textBox">
+            <p>
+              You can even bury people (or mobs) whose old selves are gone. Chickens, sheep, villagers many of them fell victim to krystek_to_ziom. We’ve got a whole graveyard. RIP legends 💀
+            </p>
+          </div>
+          <img src="/src/img/8.jpeg" alt="Graveyard" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/9.png" alt="Building fail 1" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              Building fails happen all the time, especially with axes 😅 Highly recommend trying it out. Pure chaos.
+            </p>
+          </div>
+          <img src="/src/img/10.png" alt="Building fail 2" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/11.jpeg" alt="Mod fun 1" className="home__image" />
+          <img src="/src/img/12.png" alt="Mod fun 2" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              Christian mods? Hilarious. The fun you can have while roleplaying religion 10/10 experience.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/13.jpeg" alt="Animals 1" className="home__image" />
+          <img src="/src/img/14.jpeg" alt="Animals 2" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              Animals play a huge role in the Minecraft world. They’re important and should never be harmed!
+            </p>
+          </div>
+          <img src="/src/img/15.png" alt="Animals 3" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <div className="home__textBox">
+            <p>
+              Glitched villagers are just as valuable! No harm should come to them they’re unique and irreplaceable.
+            </p>
+          </div>
+          <img src="/src/img/16.png" alt="Glitched villager 1" className="home__image" />
+          <img src="/src/img/17.jpeg" alt="Glitched villager 2" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <img src="/src/img/18.png" alt="Community 1" className="home__image" />
+          <img src="/src/img/19.png" alt="Community 2" className="home__image" />
+          <div className="home__textBox">
+            <p>
+              This game connects the weirdest and most wonderful people. Absolute chaos but in the best way possible.
+            </p>
+          </div>
+          <img src="/src/img/20.jpeg" alt="Community 3" className="home__image" />
+        </div>
+      </section>
+
+      <section className="home__section">
+        <div className="home__content">
+          <div className="home__textBox">
+            <p>
+              Minecraft is deeply religious. We’ve got churches, cemeteries, even Saint Francis himself!
+            </p>
+          </div>
+          <img src="/src/img/21.png" alt="Religious build" className="home__image" />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
