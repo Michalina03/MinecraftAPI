a5bbf135fba9e4867367f27f60988292539863b0
Michalina Janowska <janowska.michalina208@gmail.com>
2025-05-29 21:48:15 +0200

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/MinecraftAPI/',
   plugins: [react()],
   server: {
     proxy: {
